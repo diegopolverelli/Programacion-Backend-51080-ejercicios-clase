02ee9fa3c960ec2b71d690897cb0e683d4d41f66
Diego Polverelli <diegopolverelli@hotmail.com>
2023-05-20 14:50:08 -0300

    clase 19 added

---
+++ clase19_20230520/01-altaUsuarios/src/routes/views.router.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import usersModel from "../models/User.js";
+
+const router = Router();
+
+router.get('/',(req,res)=>{
+    res.render('home');
+})
+
+router.get('/users', async (req,res)=>{
+    const result = await usersModel.find().lean();
+    console.log(result)
+    res.render('users',{users:result})
+})
+
+export default router;
